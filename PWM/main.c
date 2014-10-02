5c96850dd0f2b2006bf7daf1ec6afdf417f4bf6c
lanlongzhiwang <lanlongzhiwang@gmail.com>
2014-10-03 02:27:52 +0800

    Fourth try. Back to gs004, fix all the error and get the 3 phase work, but still need some improve

--- PWM/main.c
+++ PWM/main.c
@@ -9,30 +9,22 @@
 #include "adc10.h"
 
 
-#define FCY 20000000 // 20 MIPS this is the amount of processes per second
-#define FPWM 20000 // 20 kHz switching frequency
-#define DEADTIME (unsigned int)(0.000002 * FCY) //dead time between triggers
-#define _0_DEGREES 0x0000
-#define _120_DEGREES 0x5555
-#define _240_DEGREES 0xAAAA
-#define _DES_FREQ 60 //target frequency
+#define FCY 20000000            // 20 MIPS this is the amount of processes per second
+#define RATED_FREQUENCY 50
+#define FPWM 20000              // 20 kHz switching frequency
+#define _0_DEGREES 0x0000       // 0 degree phase
+#define _120_DEGREES 0x5555     // 120 degree phase
+#define _240_DEGREES 0xAAAA     // 240 degree phase
+#define _DES_FREQ (float) 60    //target frequency
 #define _DELTA_PHASE (unsigned int)(_DES_FREQ * 65536 / FPWM)
-
-_FOSC(CSW_FSCM_OFF & XT_PLL4);      // Turn off clock switching and
-                                    // fail-safe clock monitoring and
-                                    // use the XT osc and 4x PLL as
-                                    // system clock
-_FWDT(WDT_OFF);                     // Turn off Watchdog Timer
-_FBORPOR(PBOR_OFF & MCLR_EN);       // Set Brown-out Reset voltage and set Power-up Timer to 16 ms
-_FGS(CODE_PROT_OFF); 
+#define DEADTIME (unsigned int)(0.000002 * FCY)     //dead time between triggers
 
 unsigned int Phase = 0, Delta_Phase = _DELTA_PHASE;
 unsigned int Phase_Offset1, Phase_Offset2, Phase_Offset3;
 int Multiplier1, Multiplier2, Multiplier3;
 unsigned int scaler;
 float des_f = _DES_FREQ;
-int sinetable[] =
-                    {0, 402, 804, 1206, 1608, 2009, 2411, 2811,
+int sinetable[] = {0, 402, 804, 1206, 1608, 2009, 2411, 2811,
  3212, 3612, 4011, 4410, 4808, 5205, 5602, 5998,
  6393, 6787, 7180, 7571, 7962, 8351, 8740, 9127,
  9512, 9896, 10279, 10660, 11039, 11417, 11793, 12167,
@@ -100,95 +92,63 @@ int sinetable[] =
 
 void __attribute__((__interrupt__, auto_psv)) _PWMInterrupt(void){
 
-    PWMCON2bits.UDIS = 1;
+    Phase += Delta_Phase;       // Accumulate Delta_Phase in Phase variable
+    Phase_Offset1 = _0_DEGREES; // Add proper value to phase offset (0 degree)
+    Multiplier = sinetable[(Phase + Phase_Offset1) >> 10]; // Take sine info
+    asm("MOV _Multiplier, W4"); // Load first multiplier
+    asm("MOV _PTPER, W5");      // Load second multiplier
+    asm("MOV #_Result, W0");    // Load W0 with the address of Result
+    asm("MPY W4*W5, A");        // Perform Fractional multiplication
+    asm("SAC A, [W0]");         // Store multiplication result in var Result
+    PDC1 = Result + PTPER;      // Remove negative values of the duty cycle
 
-    Phase += Delta_Phase; // ?Delta_Phase ???Phase ???
+    Phase_Offset2 = _120_DEGREES; 
+    Multiplier = sinetable[(Phase + Phase_Offset2) >> 10];
+    asm("MOV _Multiplier, W4"); 
+    asm("MOV _PTPER, W5"); 
+    asm("MOV #_Result, W0"); 
+    asm("MPY W4*W5, A"); 
+    asm("SAC A, [W0]"); 
+    PDC2 = Result + PTPER; 
 
-    Phase_Offset1 = _0_DEGREES; // ?????????????
-    Multiplier1 = sinetable[(Phase + Phase_Offset1) >> 10];// ??????
-    //Result1 = Multiplier1*PTPER*scaler;
-    //PDC1 = Result1 + PTPER;
-    asm("MOV _PTPER, W4"); // Load first multiplier (PTPER)
-    asm("MOV _scaler, W5");// Load second multiplier (scaler)
-    asm("MPY W4*W5, A");// Perform fractional multiplication of PTPER by scaler
-    asm("SAC A, W5"); // Store scaler*PTPER in W5
-    asm("MOV _Multiplier1, W4"); // Load W4 with SineSample
-    asm("MPY W4*W5, A"); // Perform Fractional multiplication of SineSample by PTPER*scaler
-    asm("ADD W5,A"); // Add PTPER*scaler to SineSample*PTPER*scaler
-    asm("SAC A,W5");
-    asm("MOV W5,PDC1");
+    Phase_Offset3 = _240_DEGREES; 
+    Multiplier = sinetable[(Phase + Phase_Offset3) >> 10];
+    asm("MOV _Multiplier, W4");
+    asm("MOV _PTPER, W5"); 
+    asm("MOV #_Result, W0"); 
+    asm("MPY W4*W5, A"); 
+    asm("SAC A, [W0]"); 
+    PDC3 = Result + PTPER; 
 
-    Phase_Offset2 = _120_DEGREES; // ?????????????
-    Multiplier2 = sinetable[(Phase + Phase_Offset2) >> 10];
-    asm("MOV _PTPER, W4"); // Load first multiplier (PTPER)
-    asm("MOV _scaler, W5");// Load second multiplier (scaler)
-    asm("MPY W4*W5, A");// Perform fractional multiplication of PTPER by scaler
-    asm("SAC A, W5"); // Store scaler*PTPER in W5
-    asm("MOV _Multiplier2, W4"); // Load W4 with SineSample
-    asm("MPY W4*W5, A"); // Perform Fractional multiplication of SineSample by PTPER*scaler
-    asm("ADD W5,A"); // Add PTPER*scaler to SineSample*PTPER*scaler
-    asm("SAC A,W5");
-    asm("MOV W5,PDC2");
-
-    Phase_Offset3 = _240_DEGREES; // ?????????????
-    Multiplier3 = sinetable[(Phase + Phase_Offset3) >> 10];
-    asm("MOV _PTPER, W4"); // Load first multiplier (PTPER)
-    asm("MOV _scaler, W5");// Load second multiplier (scaler)
-    asm("MPY W4*W5, A");// Perform fractional multiplication of PTPER by scaler
-    asm("SAC A, W5"); // Store scaler*PTPER in W5
-    asm("MOV _Multiplier3, W4"); // Load W4 with SineSample
-    asm("MPY W4*W5, A"); // Perform Fractional multiplication of SineSample by PTPER*scaler
-    asm("ADD W5,A"); // Add PTPER*scaler to SineSample*PTPER*scaler
-    asm("SAC A,W5");
-    asm("MOV W5,PDC3");
-
-    PWMCON2bits.UDIS = 0;
-    IFS2bits.PWMIF = 0;
 }
 
 void InitMCPWM(void){
-
-    TRISB = 0x00ff;
-    TRISD = 0x0003;
-    TRISE = 0x0100;
-
-    CloseADC10();
-    ADPCFG = 0xFFFF;
-
-    IFS0bits.T1IF = 0;
-    OpenTimer1(T1_ON & T1_GATE_OFF & T1_PS_1_256 & T1_SYNC_EXT_OFF & T1_SOURCE_INT, T1VAL);
-    ConfigIntTimer1(T1_INT_PRIOR_7 & T1_INT_ON);
-
-    PTPER = ((FCY/FPWM) >> 1) - 1;
-    SEVTCMP = 0;
-    PTMR=0;
-
-    PWMCON1 = 0x0077;
-    PWMCON2 = 0x0011;
-
-    DTCON1 = _DELTA_PHASE;
-    OVDCON = 0x3F00;
-
-    PDC1 = PTPER;
-    PDC2 = PTPER;
-    PDC3 = PTPER;
-
-    IFS2bits.PWMIF = 0;
-    IEC2bits.PWMIE = 1;
-
-    PTCONbits.PTEN = 1;
-    PTCONbits.PTCKPS=0;
-    PTCONbits.PTMOD=2;
-
+    TRISE = 0x0100;                     // PWM pins as outputs, and FLTA as input
+    PTPER = (FCY/FPWM - 1) >> 1;        // Compute Period for desired frequency
+    OVDCON = 0x0000;                    // Disable all PWM outputs.
+    DTCON1 = DEADTIME;                  // ~2 us of dead time @ 20 MIPS and 1:1 Prescaler
+    PWMCON1 = 0x0077;                   // Enable PWM output pins and enable complementary mode
+    PDC1 = PTPER;                       /* 0 Volts on Phase A. This value corresponds to 
+                                           50% of duty cycle, which in complementary mode 
+                                           gives an average of 0 Volts */
+    PDC2 = PTPER;                       // 0 Volts on Phase B.
+    PDC3 = PTPER;                       // 0 Volts on Phase C.
+    IFS2bits.PWMIF = 0;                 // Clear PWM Interrupt flag
+    IEC2bits.PWMIE = 1;                 // Clear PWM Interrupt flag
+    OVDCON = 0x3F00;                    // PWM outputs are controller by PWM module
+    PTCONbits.PTMOD = 2;                // Center aligned PWM operation
+    Phase = 0;                          // Reset Phase Variable
+    Delta_Phase = _DELTA_PHASE;         // Initialize Phase increment for 60Hz sine wave
+    PTCONbits.PTEN = 1;                 // Start PWM
     scaler = 32767*des_f/RATED_FREQUENCY;
-
+    return;
 }
 
 int main() {
 
     InitMCPWM();
 
-    while(1);
+    while(1){};
 
     return 0;
 }

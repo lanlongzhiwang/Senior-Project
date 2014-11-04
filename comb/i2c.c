da38fe86705ee901a8c4c2433d54cb0827b123bb
lanlongzhiwang <lanlongzhiwang@gmail.com>
2014-11-04 15:20:55 -0500

    comb var 0.5
    Be able to read current voltage, frequenct and control both.

--- comb/i2c.c
+++ comb/i2c.c
@@ -15,6 +15,7 @@ int main(int argc, char **argv) {
 	char i2c_device[]="/dev/i2c-1";
 
 	unsigned char buffer[2];
+	unsigned char rbuffer[1];
 
 	/* Open i2c device */
 	fd = open(i2c_device, O_RDWR);
@@ -28,20 +29,26 @@ int main(int argc, char **argv) {
 
 	while(1){
 
-        printf("1. Vlotage\n2. Frequency\nWhich one you want change: ");
+        printf("1. Voltage\n2. Frequency\nWhich one do you want to change: ");
         scanf("%d",&i);
         if(i == 1){
             buffer[0] = 0x09;
+            if ( (write(fd, buffer, 1)) != 1) fprintf(stderr,"Error to writing!\n");
+            if ( (read(fd, rbuffer, 1)) != 1) fprintf(stderr,"Error to reading!\n");
+            printf("The current Voltage is: %d Volt\n", rbuffer[0]);
     	    printf("Please input the Voltage(24 - 60): ");
     	    scanf("%d",&buffer[1]);
-    	    if ( (write(fd, buffer, 2)) != 2) fprintf(stderr,"Error to writting!\n");
+    	    if ( (write(fd, buffer, 2)) != 2) fprintf(stderr,"Error to writing!\n");
     	    printf("\n");
         }
         else if(i == 2){
             buffer[0] = 0x07;
+            if ( (write(fd, buffer, 1)) != 1) fprintf(stderr,"Error to writing!\n");
+            if ( (read(fd, rbuffer, 1)) != 1) fprintf(stderr,"Error to reading!\n");
+            printf("The current Frequency is: %d Hz\n", rbuffer[0]);
     	    printf("Please input the frequency(10 - 60): ");
     	    scanf("%d",&buffer[1]);
-    	    if ( (write(fd, buffer, 2)) != 2) fprintf(stderr,"Error to writting!\n");
+    	    if ( (write(fd, buffer, 2)) != 2) fprintf(stderr,"Error to writing!\n");
     	    printf("\n");
         }
 
